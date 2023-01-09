a1b16cbb7c8ffd954fc4e2953906e490351b910d
Anderson Castellon <anderson.castellon@jdoutstanding.com>
2023-01-09 10:13:35 -0600

    Add try catch statement to number animation code to avoid js errors

--- src/js/main.js
+++ src/js/main.js
@@ -88,22 +88,26 @@ const elements = document.querySelectorAll('.icon-card .icon-card__amount');
 
 if (elements && elements.length > 0) {
   elements.forEach((element) => {
-    const start = 0;
-    const end = parseInt(element.getAttribute('data-end'));
-    const duration = parseInt(element.getAttribute('data-duration'));
-    const increment = parseInt(element.getAttribute('data-increment'));
+    try {
+      const start = 0;
+      const end = parseInt(element.getAttribute('data-end'));
+      const duration = parseInt(element.getAttribute('data-duration'));
+      const increment = parseInt(element.getAttribute('data-increment'));
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        const animated = element.getAttribute('data-animated').toString() === 'true';
-        if (!animated && entry.isIntersecting) {
-          numberAnimation(element, start, end, duration, increment);
-          element.setAttribute('data-animated', 'true');
-        }
-      })
-    }, { threshold: .5 });
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          const animated = element.getAttribute('data-animated').toString() === 'true';
+          if (!animated && entry.isIntersecting) {
+            numberAnimation(element, start, end, duration, increment);
+            element.setAttribute('data-animated', 'true');
+          }
+        })
+      }, { threshold: .5 });
 
-    observer.observe(element);
+      observer.observe(element);
+    } catch (error) {
+      console.log(`Number animation error: `, error)
+    }
   })
 }
 // NUMBER COUNTER ANIMATION
@@ -153,5 +157,3 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   }
 })
-
-
